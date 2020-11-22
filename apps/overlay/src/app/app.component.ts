16e92c291bb2039cafdbbcb50b2835c77446de6f
Martin Raymond <codephobia@hotmail.com>
2020-11-22 11:21:31 -0800

    feat: getting initial docker images setup

--- apps/overlay/src/app/app.component.ts
+++ apps/overlay/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pool-overlay-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'pool-overlay-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'overlay';
-}
+export class AppComponent { }
