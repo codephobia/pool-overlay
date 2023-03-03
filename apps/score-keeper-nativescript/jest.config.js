4b628b7b9befb73c3582642d97b45681196fef94
Martin Raymond <codephobia@hotmail.com>
2023-03-03 14:45:55 -0800

    feat(score-keeper): initial nativescript setup for application

---
+++ apps/score-keeper-nativescript/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: '../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            astTransformers: [
+                'jest-preset-angular/build/InlineFilesTransformer',
+                'jest-preset-angular/build/StripStylesTransformer',
+            ],
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+        },
+    },
+    coverageDirectory: '../../coverage/apps/score-keeper-nativescript',
+
+    displayName: 'nativescript-safety',
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment',
+    ],
+};
